27c9cbb4ca05b02754c2a04c7cf43facc5884fe2
Giles Thomas <giles@giles.net>
2019-11-25 17:59:53 +0000

    Python 3 compatibility.  by: Glenn, Giles

--- secureconfig/secureconfigparser.py
+++ secureconfig/secureconfigparser.py
@@ -5,13 +5,16 @@ import sys
 import cryptography
 
 from .baseclass import cryptkeeper_access_methods
-from ConfigParser import ConfigParser, NoSectionError, NoOptionError
+try:
+    from configparser import ConfigParser, NoSectionError, NoOptionError
+except ModuleNotFoundError:
+    from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 
 
 # SECURECONFIG pattern:
 #  - allow key retrieval and storage from CryptKeeper object
 #  - default to symmetric keys using AES (via Fernet)
-#  - (future) provide asymmetric encryption using RSA 
+#  - (future) provide asymmetric encryption using RSA
 
 # SECURECONFIGPARSER pattern:
 #  - read list of files
@@ -28,19 +31,19 @@ from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 # ConfigParser is an "old style" class, so we're using old-style calls to super
 # until ConfigParser gets its act together.
 
-# oh god multiple inheritance 
+# oh god multiple inheritance
 # /me crosses her fingers
 
 class SecureConfigParser(ConfigParser, cryptkeeper_access_methods):
     '''A subclass of ConfigParser py:class::ConfigParser which decrypts certain entries.'''
 
     def __init__(self, *args, **kwargs):
-        
+
         # supplied by cryptkeeper_access_methods
-        self.ck = kwargs.pop('ck', None) 
-        
+        self.ck = kwargs.pop('ck', None)
+
         ConfigParser.__init__(self, *args, **kwargs)
-    
+
     def read(self, filenames):
         '''Read the list of config files.'''
         #print("[DEBUG] filenames: ", filenames)
@@ -55,10 +58,10 @@ class SecureConfigParser(ConfigParser, cryptkeeper_access_methods):
             return default
         except Exception as e:
             print("[DEBUG]", sys.exc_info()[0])
-            
+
     def raw_set(self, sec, key, val):
         '''Set the value without encrypting it.'''
-        return ConfigParser.set(self, sec, key, val) 
+        return ConfigParser.set(self, sec, key, val)
 
     def raw_items(self, sec):
         '''Return the items in a section without decrypting the values.'''
@@ -81,7 +84,7 @@ class SecureConfigParser(ConfigParser, cryptkeeper_access_methods):
         return val
 
     def set(self, sec, key, new_val, encrypt=False):
-        '''If the value should be secured, encrypt and update it; 
+        '''If the value should be secured, encrypt and update it;
             Otherwise just update it.  supply encrypt=True to encrypt
             a value that was not previously encrypted.
         '''
@@ -89,7 +92,7 @@ class SecureConfigParser(ConfigParser, cryptkeeper_access_methods):
             if encrypt==True:
                 new_val = self.ck.sigil + self.ck.encrypt(new_val)
             return self.raw_set(sec, key, new_val)
-        
+
         old_raw_val = self.raw_get(sec, key)
 
         if old_raw_val.startswith(self.ck.sigil) or encrypt==True:
